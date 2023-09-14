1134220a722fa1d58256537f5700e480d685b439
Lucas Manço de Souza <lucasm.souza@teclat.com.br>
2023-09-14 02:48:09 -0300

    Ajuste erro na logica de storeids

--- Block/Seo.php
+++ Block/Seo.php
@@ -2,32 +2,73 @@
 namespace LucasManco\MSSeoFixer\Block;
 
 use Magento\Framework\View\Element\Template;
-use Magento\Framework\View\Element\Template\Context;
-use Magento\Cms\Model\Page;
-use Magento\Store\Model\StoreManagerInterface;
 
 class Seo extends Template
 {
-    
+    private $storeManager;
+    private $pageModel;
+    private $context;
+
     public function __construct(
-        Context $context,
+        \Magento\Framework\View\Element\Template\Context $context,
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+        \Magento\Cms\Model\Page $pageModel,
         array $data = []
     ) {
+        $this->storeManager = $storeManager;
+        $this->pageModel = $pageModel;
+        $this->context = $context;
         parent::__construct($context, $data);
-    
-    }
-    public function getCurrentUrl(){
-        $currentUrl = $this->getUrl('*/*/*', ['_current' => true, '_use_rewrite' => true]);
-
-        return $currentUrl;
-    }
-
-    public function getStoreId(){
-        $objectManager =  \Magento\Framework\App\ObjectManager::getInstance();        
-        $storeManager = $objectManager->get('\Magento\Store\Model\StoreManagerInterface');
         
-        $storeId =  $storeManager->getStore()->getCode();
-        return $storeId;
     }
+    public function getUrls($storeViews){
+        
+        $baseUrls = [];
+        foreach ($storeViews as $storeview) {
+            $store = $this->storeManager->getStore($storeview);
+            $baseUrls[] = $store->getBaseUrl();
+        }
+        return $baseUrls;
+    }
+
+    public function getStoreIds(){
+        
+        $pageId = $this->getRequest()->getParam('page_id');
+
+        // die(json_encode($pageId));
+        $page = $this->pageModel->load($pageId);
+        
+        $storeIds = $page->getStoreId();
+        // die(json_encode($storeIds));
+        if (!$storeIds) {
+            return false;
+        }
+        //storeid = 0 => all store views
+        if ($storeIds[0] == 0){
+            $storeManagerDataList = $this->_storeManager->getStores();
+            // die(json_encode($storeManagerDataList));
+            foreach ($storeManagerDataList as $storeId=>$store) {
+                $storeViews[] = $store->getCode();
+            }
+        }
+        else{
+            foreach ($storeIds as $storeId) {
+                $store = $this->storeManager->getStore($storeId);
+                $storeViews[] = $store->getCode();
+            }
+        }
+        
+
+        
+        return $storeViews;
+    }
+    public function getCmsPageUrl(){
+        $pageId = $this->getRequest()->getParam('page_id');
+        $page = $this->pageModel->load($pageId);
+        $cmsPageUrl = $page->getIdentifier();
+    
+        return $cmsPageUrl;
+    }
+
 }
 
